b190567299a1b41c5c5ce087750d8ecb0ee26a08
FatihKaraoglu <karaoglu.fatih16@gmail.com>
2024-07-29 23:18:32 +0200

    slick-slider implemented

--- DateSwipe.Client/wwwroot/js/slick-Init.js
+++ DateSwipe.Client/wwwroot/js/slick-Init.js
@@ -1,14 +1,19 @@
-﻿window.initializeSlick = () => {
-    console.log("Initializing Slick slider...");
-    $('.slick-slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: true,
-        infinite: true,
-        speed: 300,
-        arrows: true,
-        nextArrow: '<button type="button" class="slick-next">Next</button>',
-        prevArrow: '<button type="button" class="slick-prev">Prev</button>',
+﻿function onDocumentReady() {
+    $(document).ready(function () {
+        console.log("Initializing Slick slider...");
+        $('.slick-slider').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: true,
+            infinite: true,
+            speed: 300,
+            arrows: true,
+            nextArrow: '<button type="button" class="slick-next">Next</button>',
+            prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+        });
     });
-};
+}
+
+window.initializeSlick = onDocumentReady;
+
 
